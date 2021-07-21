6f3e152935ed2373ebf50d5876a46e15ab752cca
ommemusic <ommemusicweb@gmail.com>
2021-07-21 19:19:56 +0200

    go

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 /*=============================================
 =            Navbar                           =
 =============================================*/
-const menuToggle = document.querySelector("#menu-toggle");
+/*const menuToggle = document.querySelector("#menu-toggle");
 const menuClose = document.querySelector("#menu-close");
 const menu = document.querySelector("#menu");
 
@@ -12,4 +12,300 @@ menuToggle.addEventListener("click", () => {
 menuClose.addEventListener("click", () => {
   menu.classList.remove("menu-open");
 });
-/*=====  End of Navbar                 ======*/
+=====  End of Navbar                 ======*/
+
+/*=============================================
+=            Swiper                           =
+=============================================*/
+/* Top */
+const swiperTop = new Swiper(".swiper-top", {
+  autoplay: {
+    delay: 5000,
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 16,
+    },
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  slidesPerView: 3,
+  spaceBetween: 6,
+});
+
+/* Lombardia */
+const swiperLombardia = new Swiper(".swiper-lombardia", {
+  autoplay: {
+    delay: 7000,
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 16,
+    },
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  reverseDirection: true,
+  slidesPerView: 3,
+  spaceBetween: 6,
+});
+
+/* Veneto */
+const swiperVeneto = new Swiper(".swiper-veneto", {
+  autoplay: {
+    delay: 7000,
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 16,
+    },
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  slidesPerView: 3,
+  spaceBetween: 6,
+});
+
+/* Extra */
+const swiperExtra = new Swiper(".swiper-extra", {
+  autoplay: {
+    delay: 7000,
+  },
+  breakpoints: {
+    1024: {
+      spaceBetween: 16,
+    },
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  slidesPerView: 3,
+  spaceBetween: 6,
+});
+
+/* Cerimonia */
+const swiperCerimonia = new Swiper(".swiper-cerimonia", {
+  autoplay: {
+    delay: 7000,
+  },
+  breakpoints: {
+    1024: {
+      spaceBetween: 16,
+    },
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  slidesPerView: 3,
+  spaceBetween: 6,
+});
+
+/* Ricevimento */
+const swiperRicevimento = new Swiper(".swiper-ricevimento", {
+  autoplay: {
+    delay: 7000,
+  },
+  breakpoints: {
+    1024: {
+      spaceBetween: 16,
+    },
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  slidesPerView: 3,
+  spaceBetween: 6,
+});
+
+/* Animazione */
+const swiperAnimazione = new Swiper(".swiper-animazione", {
+  autoplay: {
+    delay: 7000,
+  },
+  breakpoints: {
+    1024: {
+      spaceBetween: 16,
+    },
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  slidesPerView: 3,
+  spaceBetween: 6,
+});
+
+/* Intrattenimento */
+const swiperIntrattenimento = new Swiper(".swiper-intrattenimento", {
+  autoplay: {
+    delay: 7000,
+  },
+  breakpoints: {
+    1024: {
+      spaceBetween: 16,
+    },
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  slidesPerView: 3,
+  spaceBetween: 6,
+});
+
+/* Effetti */
+const swiperEffetti = new Swiper(".swiper-effetti", {
+  autoplay: {
+    delay: 7000,
+  },
+  breakpoints: {
+    1024: {
+      spaceBetween: 16,
+    },
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  slidesPerView: 3,
+  spaceBetween: 6,
+});
+
+/* Abbinamenti */
+const swiperAbbinamenti = new Swiper(".swiper-pairings", {
+  autoplay: {
+    delay: 7000,
+  },
+  breakpoints: {
+    1024: {
+      spaceBetween: 16,
+    },
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  slidesPerView: 4,
+  spaceBetween: 6,
+});
+
+/* Video */
+const swiperVideo = new Swiper(".swiper-video", {
+  autoplay: {
+    delay: 7000,
+  },
+  breakpoints: {
+    1024: {
+      spaceBetween: 16,
+    },
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  slidesPerView: 4,
+  spaceBetween: 6,
+});
+
+/* Artists */
+const swiperArtists = new Swiper(".swiper-artists", {
+  autoplay: {
+    delay: 7000,
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 16,
+    },
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  reverseDirection: true,
+  slidesPerView: 3,
+  spaceBetween: 6,
+});
+
+/* Foto */
+const swiperFoto = new Swiper(".swiper-foto", {
+  autoplay: {
+    delay: 7000,
+  },
+  breakpoints: {
+    1024: {
+      spaceBetween: 16,
+    },
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  slidesPerView: 7,
+  spaceBetween: 6,
+});
+/*=====  End of Swiper                 ======*/
